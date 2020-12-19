7ba27c2d95f9cc33b146af38ad9606bf26464e41
Kritik Modi <kritikmodi@gmail.com>
2020-12-19 20:06:15 +0530

    Upload files

---
+++ scanner_app/lib/homePage1.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'scan.dart';
+
+class homePage1 extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<homePage1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Smart Parking System", style: TextStyle(
+        fontFamily: 'montserrat1')),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image(image: NetworkImage("https://media.istockphoto.com/vectors/qr-code-scan-phone-icon-in-comic-style-scanner-in-smartphone-vector-vector-id1166145556")),
+            flatButton("Scan QR CODE", ScanPage()),
+            SizedBox(height: 20.0,),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget flatButton(String text, Widget widget) {
+    return FlatButton(
+      padding: EdgeInsets.all(15.0),
+      onPressed: () async {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => widget));
+      },
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+      ),
+      shape: RoundedRectangleBorder(
+          side: BorderSide(color: Colors.black,width: 3.0),
+          borderRadius: BorderRadius.circular(20.0)),
+    );
+  }
+}
---
+++ scanner_app/lib/signup.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+
+import 'homePage1.dart';
+
+class signup extends StatefulWidget {
+  @override
+  signupstate createState() => signupstate();
+}
+
+class signupstate extends State<signup> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController repasswordController = TextEditingController();
+  //irebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text('CODE SHINOBIS',style: TextStyle(fontFamily: 'painter'))
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Smart Parking System',
+                      style: TextStyle(
+                            fontFamily: 'montserrat1',
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontFamily: 'montserrat', fontSize: 20),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
+                  ),
+                ),
+
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User email',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'New Password',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: repasswordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Re-enter Password',
+                    ),
+                  ),
+                ),
+
+                Container(
+                    height: 70,
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.black,
+                      child: Text('Sign Up', style: TextStyle(fontFamily: 'montserrat1')),
+                      onPressed: () async {
+
+
+                        if(passwordController.text!='' && nameController.text!='' && emailController.text!='' && (passwordController.text==repasswordController.text))
+                        {
+
+                         /* try {
+                            UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                                email: emailController.text,
+                                password: passwordController.text);
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> homePage1()),
+                            );*/
+                            Fluttertoast.showToast(
+                                msg: "Sign up successsful!",
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.BOTTOM,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: Colors.blue,
+                                textColor: Colors.white,
+                                fontSize: 16.0
+                            );
+                          /*} on FirebaseAuthException catch (e) {
+                            if (e.code == 'weak-password') {
+                              Fluttertoast.showToast(
+                                  msg: "Password too weak!",
+                                  toastLength: Toast.LENGTH_LONG,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 1,
+                                  backgroundColor: Colors.blue,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0
+                              );
+                            } else if (e.code == 'email-already-in-use') {
+                              Fluttertoast.showToast(
+                                  msg: "An account already exists for this email!",
+                                  toastLength: Toast.LENGTH_LONG,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 1,
+                                  backgroundColor: Colors.blue,
+                                  textColor: Colors.white,
+                                  fontSize: 16.0
+                              );
+                            }
+                          } catch (e) {
+                            print(e);
+                          }*/
+
+                        }
+                        else {
+                          Fluttertoast.showToast(
+                              msg: "Please enter all the fields and the passwords correctly!",
+                              toastLength: Toast.LENGTH_LONG,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 1,
+                              backgroundColor: Colors.blue,
+                              textColor: Colors.white,
+                              fontSize: 16.0
+                          );
+                        }
+                      },// onPressed
+                    )),
+
+              ],
+            )));
+  }
+}
